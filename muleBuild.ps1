c2590b2c83068a0cf6cd9039e7ad60d51492d45b
Rob <kritz.rob@gmail.com>
2022-09-14 10:25:54 +0200

    update

--- muleBuild.ps1
+++ muleBuild.ps1
@@ -5,18 +5,8 @@ Write-host Building Mule Project with Maven
 Write-host ---------------------------------------------------
 Write-host
 
-$target = Test-Path -Path 'target/'
-
-if (!$target){
-    Write-Host "No target file found. Are you in the project root?"
-    Write-Host
-    exit
-}
-
 try {
     mvn clean package -DskipTests=true
-    Get-ChildItem -Path "target/" -File | Rename-Item -NewName { $_.BaseName -replace "\d-SNAPSHOT-mule-application", ($v + "-" + $r + $_.Extension) } 
-    ii ./target
 } catch {
     Write-Output "An Error occurred during build"
     Write-Host
