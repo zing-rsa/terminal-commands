afb2f695d33cf11edc81eab03b110ae4e37ea8e4
Rob <kritz.rob@gmail.com>
2023-06-06 12:12:02 +0200

    update gt

--- gt.ps1
+++ gt.ps1
@@ -1,24 +1,23 @@
 param (
-    [string]$n = "default"
+    [string]$n  = "default",
+    [switch]$ls = $false
 )
-if ($n -eq "vicitude"){
-    Set-Location C:\users\kritz\documents\vicitude
-}elseif($n -eq "desktop"){
-    Set-Location C:\users\kritz\desktop
-}elseif($n -eq "landing"){
-    Set-Location C:\users\kritz\documents\landing
-}elseif($n -eq "docs"){
-    Set-Location C:\users\kritz\documents
-}elseif($n -eq "downloads"){
-    Set-Location C:\users\kritz\downloads
-}elseif($n -eq "projects"){
-    Set-Location C:\users\kritz\documents\Code_Projects
-}elseif($n -eq "libfin"){
-    Set-Location C:\users\kritz\documents\vicitude\LibfinRepos
-}elseif($n -eq "temp"){
-    Set-Location C:\users\kritz\desktop\temp
-}elseif($n -eq "submit-api"){
-    Set-Location 'C:\Program Files\cardano-node-1.33.0-win64'
-}else{
-    write-output "Not found"
+
+$locations = @{
+    vicitude  = "C:\users\kritz\documents\vicitude"
+    desktop   = "C:\users\kritz\desktop"
+    landing   = "C:\users\kritz\documents\landing"
+    docs      = "C:\users\kritz\documents"
+    downloads = "C:\users\kritz\downloads"
+    projects  = "C:\users\kritz\documents\Code_Projects"
+    libfin    = "C:\users\kritz\documents\vicitude\LibfinRepos"
+    temp      = "C:\users\kritz\desktop\temp"
+}
+
+if ($ls) {
+    $locations
+} elseif ($locations.ContainsKey($n)) {
+    Set-Location $locations.$n
+} else {
+    Write-Output "Location key not found."
 }
